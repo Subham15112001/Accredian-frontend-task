b14d588f008f6fb0bbf312bf9a06b5dbfc058666
Subham15112001 <sinchan889377888@gmail.com>
2024-07-24 19:39:14 +0530

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,11 +4,11 @@ import conf from '../conf/conf'
 const BASE_URL = conf.backendUrl;
 
 export const api =  axios.create({
-    baseURL: "https://accredian-backend-task-production-4ace.up.railway.app"
+    baseURL: "https://accredian-backend-task-production-4ace.up.railway.app/api"
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: "https://accredian-backend-task-production-4ace.up.railway.app",
+    baseURL: "https://accredian-backend-task-production-4ace.up.railway.app/api",
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 });
