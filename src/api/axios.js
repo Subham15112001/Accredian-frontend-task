0faf973152fe9865bc33b823ad1d887e4b73d3b8
Subham15112001 <sinchan889377888@gmail.com>
2024-07-24 18:41:50 +0530

    url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import conf from '../conf/conf'
 const BASE_URL = conf.backendUrl;
 
 export const api =  axios.create({
-    baseURL: BASE_URL
+    baseURL: "https://accredian-backend-task-production-4ace.up.railway.app"
 });
 
 export const axiosPrivate = axios.create({
